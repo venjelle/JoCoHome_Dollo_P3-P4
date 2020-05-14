a3d3acddb2a6c8de50219ba7bc9deda48fe9f90c
145379 <145379@student.horizoncollege.nl>
2020-05-14 13:55:52 +0200

    Created ASP.NET Core mvc project

---
+++ JoCoHome_Dollo/JoCoHome_Dollo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JoCoHome_Dollo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
