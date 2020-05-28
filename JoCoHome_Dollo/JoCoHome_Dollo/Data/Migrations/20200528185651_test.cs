63e63ba307472bcc1b20764594231f2f3bd9935b
jobkuijper <56111459+jobkuijper@users.noreply.github.com>
2020-05-28 21:41:45 +0200

    contact pagina aangemaakt en logo toegevoegd

---
+++ JoCoHome_Dollo/JoCoHome_Dollo/Data/Migrations/20200528185651_test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JoCoHome_Dollo.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naam = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    TelefoonNr = table.Column<string>(nullable: false),
+                    Onderwerp = table.Column<string>(nullable: false),
+                    Bericht = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
---
+++ JoCoHome_Dollo/JoCoHome_Dollo/Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoCoHome_Dollo.Models
+{
+    public class Contact
+    { 
+     public int Id { get; set; }
+
+    [Required]
+    public string Naam { get; set; }
+
+    [Required]
+    [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
+    public string Email { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[0-9]*$")]
+    [Display(Name = "Telefoon Nr")]
+    public string TelefoonNr { get; set; }
+
+    [Required]
+    public string Onderwerp { get; set; }
+
+    [Required]
+    public string Bericht { get; set; }
+
+    }
+}
