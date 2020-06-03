3ef52ff0a6881315e36701a7e0f324ac42cd16a6
jobkuijper <56111459+jobkuijper@users.noreply.github.com>
2020-06-03 14:34:25 +0200

    admin account fuxxed

---
+++ JoCoHome_Dollo/JoCoHome_Dollo/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JoCoHome_Dollo.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoCoHome_Dollo.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext _context;
+
+        RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+
+
+        }
+
+        [Area("Admin")]
+
+        [Authorize(Roles = "Admin")]
+
+
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
--- JoCoHome_Dollo/JoCoHome_Dollo/Startup.cs
+++ JoCoHome_Dollo/JoCoHome_Dollo/Startup.cs
@@ -30,7 +30,7 @@ namespace JoCoHome_Dollo
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
+            services.AddDefaultIdentity<IdentityUser>().AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
