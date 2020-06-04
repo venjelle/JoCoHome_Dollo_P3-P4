182a691cccee4c721268646c601e929bf9ea4864
jobkuijper <56111459+jobkuijper@users.noreply.github.com>
2020-06-05 00:28:13 +0200

    Admin paneel werkend alleen navigatie controlleren

---
+++ JoCoHome_Dollo/JoCoHome_Dollo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JoCoHome_Dollo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JoCoHome_Dollo.Areas.Identity.IdentityHostingStartup))]
+namespace JoCoHome_Dollo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
